4359ebf4518c2b299a7ac9501a157e0014d0fb67
Carlos Azevedo <cguerraazevedo@tecnico.ulisboa.pt>
2018-08-03 16:57:59 +0100

    added pnml tools with partial pnml import and petri net drawing

--- gspn.py
+++ gspn.py
@@ -3,6 +3,8 @@ import numpy as np
 # from pntools import *
 import petrinet as pn
 import xml.etree.ElementTree as et # XML parser
+from graphviz import Digraph
+import time
 
 class gspn(object):
     '''
@@ -17,6 +19,8 @@ class gspn(object):
         self.transitions = {}
         self.arc_in = {}
         self.arc_out = {}
+        self.arc_in_m = np.array([[]])
+        self.arc_out_m = np.array([[]])
 
     def add_places(self, name, ntokens=[]):
         '''
@@ -56,11 +60,41 @@ class gspn(object):
 
         return self.transitions
 
-    def add_arc_in(self, arc):
+    def add_arcs(self, arc_in, arc_out):
         '''
         PxT represents the arc connections from places to transitions
         '''
+        # matrix = two dimentional array
+        # [
+        # [1, 2, 3],
+        # [4, 5, 6]
+        # ]
+        # column -> a[:,1]
+        # row -> a[1,:]
+        self.arc_in = arc_in
+        self.arc_out = arc_out
+
+        list_items = np.array([])
+        for source, target in arc_in.items():
+            for item in target:
+                list_items = np.append(list_items, item)
+
+        self.arc_in_m = np.zeros( (len(arc_in)+1 , len(np.unique(list_items))+1) )
+        # self.arc_in_m[0,:] =
+        # self.arc_in_m[:,0] =
+
+        # for source, target in arc_in.items():
+        #     arc_in_m
+
+        # arc_in = {}
+        # arc_in['p1'] = ['t1']
+        # arc_in['p2'] = ['t2']
+        # arc_in['p3'] = ['t3']
+        # arc_in['p4'] = ['t4']
+        # arc_in['p5'] = ['t1', 't3']
+
         # self.arc_in = arc
+
         # self.arc_in_m = np.array([])
 
         # self.arc_in_m = [0] * len(arc)
@@ -115,45 +149,130 @@ class gspn(object):
 
         return True
 
+class pnml_tools(object):
+    def __init__(self):
+        '''
+        Class constructor: will get executed at the moment
+        of object creation
+        '''
+        self.list_gspn = []  # list of parsed GSPN objects
+        self.gspn = gspn()
+
+
+    def import_pnml(self, file):
+        tree = et.parse(file)  # parse XML with ElementTree
+        root = tree.getroot()
+
+        for petrinet in root.iter('net'):
+
+            place_name = []
+            place_marking = []
+            for pl in petrinet.iter('place'): # iterate over all places of the petri net
+                place_name.append(pl.get('id')) # get place name encoded as 'id' in the pnml structure
+
+                text = pl.find('./initialMarking/value').text
+                place_marking.append(int(text.split(',')[-1])) # get place marking encoded inside 'initalMarking', as the 'text' of the key 'value'
+
+            self.gspn.add_places(place_name, place_marking) # add the compiled list of places to the gspn object
+
+            transition_name = []
+            transition_type = []
+            transition_rate = []
+            for tr in petrinet.iter('transition'): # iterate over all transitions of the petri net
+                transition_name.append(tr.get('id')) # get transition name encoded as 'id' in the pnml structure
+
+                if (tr.find('./timed/value').text == 'true'): # get the transition type either exponential ('exp') or immediate ('imm')
+                    transition_type.append('exp')
+                else:
+                    transition_type.append('imm')
+
+                transition_rate.append(float(tr.find('./rate/value').text)) # get the transition fire rate or weight
+
+            self.gspn.add_transitions(transition_name, transition_type, transition_rate)  # add the compiled list of transitions to the gspn object
+
+
+
+        return True
+
+    def export_pnml(self, file):
+
+        return True
+
+    def show_gspn(self, file='', gspn=''):
+        # shape
+        # style box rect
+        # ref: https://www.graphviz.org/documentation/
+        # check where to put forcelabels=true and  labelfloat='true'
+
+        gspn_draw = Digraph(name='gspn')
+
+        # places
+        gspn_draw.node('P0',shape='circle', label='<&#9899;>', xlabel='P0')
+        gspn_draw.node('P1',shape='circle',  label='', xlabel='P1')
+
+        # exponential transitions
+        gspn_draw.node('T0', shape='rectangle', color='black', label='', xlabel='T2')
+
+        # immediate transitions
+        gspn_draw.node('T1', shape='rectangle', style='filled', color='black', label='', xlabel='T2')
+        gspn_draw.node('T2', shape='rectangle', style='filled', color='black', label='', xlabel='T2')
+
+        gspn_draw.edge('P0','T0')
+        gspn_draw.edge('P0','T1')
+        gspn_draw.edge('P1','T2')
+        gspn_draw.edge('T0','P1')
+        gspn_draw.edge('T1','P1')
+        gspn_draw.edge('T2','P0')
+
+        gspn_draw.render('GV_gspn.gv', view=True)
+
+        time.sleep(5)
+
+        gspn_draw.node('P0', shape='circle', label='', xlabel='P0')
+        gspn_draw.node('P1', shape='circle', label='<&#9899;>', xlabel='P1')
+
+        gspn_draw.render('GV_gspn.gv', view=True)
+
+
 if __name__ == "__main__":
     # create a generalized stochastic petri net structure
     my_pn = gspn()
-    # places = my_pn.add_places(['p1', 'p2', 'p3', 'p4', 'p5'], [1, 0, 1, 0, 1])
-    places = my_pn.add_places(['p1', 'p2', 'p3', 'p4', 'p5'])
+    places = my_pn.add_places(['p1', 'p2', 'p3', 'p4', 'p5'], [1, 0, 1, 0, 1])
+    # places = my_pn.add_places(['p1', 'p2', 'p3', 'p4', 'p5'])
     trans = my_pn.add_transitions(['t1', 't2', 't3', 't4'], ['exp', 'exp', 'exp', 'exp'], [1, 1, 0.5, 0.5])
+
     arc_in = {}
     arc_in['p1'] = ['t1']
     arc_in['p2'] = ['t2']
     arc_in['p3'] = ['t3']
     arc_in['p4'] = ['t4']
     arc_in['p5'] = ['t1', 't3']
-    my_pn.add_arc_in(arc_in)
 
     arc_out = {}
     arc_out['t1'] = ['p2']
     arc_out['t2'] = ['p5', 'p1']
     arc_out['t3'] = ['p4']
     arc_out['t4'] = ['p3', 'p5']
-    my_pn.add_arc_out(arc_out)
+    mm = my_pn.add_arcs(arc_in ,arc_out)
 
-    print('Places: ' , my_pn.get_current_marking(), '\n')
-    print('Trans: ' , my_pn.get_transitions(), '\n')
-    print('Arcs IN: ' , my_pn.get_in_arcs(), '\n')
-    print('Arcs OUT: ' , my_pn.get_out_arcs(), '\n')
-
-    print(my_pn.add_tokens(['p1', 'p3', 'p5'], [10,5,1]))
-
-    print('Places: ', my_pn.get_current_marking(), '\n')
-
-    # tree = et.parse('example.pnml') # parse XML with ElementTree
-    # root = tree.getroot()
-    # print(tree)
+    # print('Places: ' , my_pn.get_current_marking(), '\n')
+    # print('Trans: ' , my_pn.get_transitions(), '\n')
+    # print('Arcs IN: ' , my_pn.get_in_arcs(), '\n')
+    # print('Arcs OUT: ' , my_pn.get_out_arcs(), '\n')
     #
-    # for net_node in root.iter('net'):
-    #     print(net_node)
+    # print(my_pn.add_tokens(['p1', 'p3', 'p5'], [10,5,1]))
+    #
+    # print('Places: ', my_pn.get_current_marking(), '\n')
 
-    nets = pn.parse_pnml_file('example.pnml')
-    # print(nets)
-    for net in nets:
-        print(nets.pop())
-        print('------------')
+    # parset = pnml_tools()
+    # mm = parset.import_pnml('pipediag.xml')
+    # print(mm)
+
+    parset = pnml_tools()
+    parset.show_gspn()
+
+    # nets = pn.parse_pnml_file('example.pnml')
+    # # print(nets)
+    # for net in nets:
+    #     print(nets.pop())
+    #     print('------------')
