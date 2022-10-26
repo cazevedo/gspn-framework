af3f6e80a2fb11826a9620e5ea9ba28e75d66a86
Carlos Azevedo <cmaz90@gmail.com>
2022-10-26 15:26:27 +0100

    refactored repo

---
+++ gspn_gym_env/__init__.py
@@ -0,0 +1,11 @@
+from gym.envs.registration import register
+
+register(
+    id='gspn-env-v0',
+    entry_point='gspn_gym_env.envs:GSPNenv',
+)
+
+register(
+    id='multi-gspn-env-v0',
+    entry_point='gspn_gym_env.envs:MultiGSPNenv',
+)
